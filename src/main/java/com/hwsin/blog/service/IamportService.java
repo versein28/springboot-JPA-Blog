fdd0b2ffd0ab63d9e294c3f9311bd68978eddd68
versein28 <versein28@gmail.com>
2022-09-05 18:07:31 +0900

    1.주문현황 2.장바구니 3.상품 검색 및 정렬 기능 추가

--- src/main/java/com/hwsin/blog/service/IamportService.java
+++ src/main/java/com/hwsin/blog/service/IamportService.java
@@ -1,21 +1,63 @@
 package com.hwsin.blog.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import java.util.HashMap;
+import java.util.Map;
 
-import com.hwsin.blog.model.Board;
-import com.hwsin.blog.model.Iamport;
-import com.hwsin.blog.model.Users;
-import com.hwsin.blog.repository.IamportRepository;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.hwsin.blog.dto.IamportDto;
 
 @Service
-public class IamportService {
+public class PaymentService {
+	@Value("${imp_key}")
+	private String imp_key;
+
+	@Value("${imp_secret}")
+	private String imp_secret;
+
+	public String GetToken() {
+		RestTemplate restTemplate = new RestTemplate();
+
+		// 서버로 요청할 Header
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("imp_key", imp_key);
+		map.put("imp_secret", imp_secret);
+
+		// 서버로 요청할 Body
+		JSONObject body =  new JSONObject(map);
+		HttpEntity<JSONObject> entity = new HttpEntity<>(body, headers);
+
+		return restTemplate.postForObject("https://api.iamport.kr/users/getToken", entity, String.class);
+	}
 	
-	@Autowired IamportRepository iamportRepository;
-	
-	@Transactional
-	public void 저장하기(Iamport iamport) { 
-		iamportRepository.save(iamport);
+	public JSONObject GetPayInfo(String token, String imp_uid) throws ParseException {
+		JSONObject objData = (JSONObject) new JSONParser().parse(token);
+		JSONObject response = (JSONObject) objData.get("response");
+		String access_token = (String) response.get("access_token");
+		
+		RestTemplate restTemplate = new RestTemplate();
+
+		// 서버로 요청할 Header
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.add("Authorization", access_token);
+		
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		
+		IamportDto iamportDto = restTemplate.postForObject("https://api.iamport.kr/payments/"+imp_uid , entity, IamportDto.class);
+		JSONObject rsp = iamportDto.getResponse();
+		
+		return rsp;
 	}
 }
